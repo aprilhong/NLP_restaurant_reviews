f44a8f25a287bfa382f5e22a93393327f0b5613c
April Hong <78663820+aprilhong@users.noreply.github.com>
2024-09-11 13:49:07 -0400

    update directory

---
+++ app.py
@@ -0,0 +1,42 @@
+# data manipulation and visualization
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+# text preprocessing
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+import string
+
+# modeling
+import pickle
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from  sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+import sklearn.metrics as metrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report,confusion_matrix, ConfusionMatrixDisplay
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title('Restaurant Reviews Classification App')
+
+# load the dataset
+def load_data():
+    df = pd.read_csv("C:\Users\tunhi\OneDrive\Documents\Github\NLP_restaurant_reviews\data\raw\Restaurant_Reviews.tsv", sep='\t')
+    return df
+
+# preprocess the data
