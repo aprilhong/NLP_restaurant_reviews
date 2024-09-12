cf7929b122f85f8a8068ea2c33640aeb5ded2e6e
April Hong <78663820+aprilhong@users.noreply.github.com>
2024-09-12 10:25:52 -0400

    update app

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ model = pickle.load(open('models/model.pkl', 'rb'))
 if submit:
     review = preprocess_text(review)
     review = vectorizer.transform([review]).toarray()
-    prediction = model.predict([review])
-    st.write(prediction[0])
+    prediction = model.predict(review)
+    st.write(prediction)
     if prediction == 1:
         st.write("We're glad you enjoyed your visit!")
     else: 
